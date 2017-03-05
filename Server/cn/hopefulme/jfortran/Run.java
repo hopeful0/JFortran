29b766976930e3d5ef9a5ab29f46361519654f12
hopeful0 <hopeful_world@163.com>
2017-03-05 18:14:14 +0800

    break loop when exception can not be caught

--- Server/cn/hopefulme/jfortran/Run.java
+++ Server/cn/hopefulme/jfortran/Run.java
@@ -5,6 +5,8 @@ import java.io.*;
 
 public class Run {
 
+	private static long lastHeart;
+
 	/**
 	 * 运行程序
 	 * @param is socket输入流，用来读取客户端输入
@@ -35,16 +37,25 @@ public class Run {
 			//执行程序
 			Terminal terminal = new Terminal("./temp/exe");
 			terminal.start();
+			lastHeart = System.nanoTime();
 	 		while(terminal.process.isAlive()) {
+				if (System.nanoTime() - lastHeart > 100000000) {
+					System.out.println("Lose Connection!");
+					break;
+				}
 				String input = "";
 				//有输入时获取输入
 				String data;
 				while(is.available() > 0 && (data=br.readLine()) != null) {
 					//忽略其中的心跳包
-					if(data.equals("**##*#*#heart package**##*#*#")) continue;
+					if(data.equals("**##*#*#heart package**##*#*#")) {
+						lastHeart = System.nanoTime();
+						continue;
+					}
 					if(data.equals("**##*#*#input ended**##*#*#")) break;
 					input += data + "\n";
 		 		}
+				if (input.length() > 0)
 					terminal.input(input);
 				String output = terminal.output();
 				//有输出时输出信息
